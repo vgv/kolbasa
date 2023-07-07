d8a882c3378515d9823b85103f2c2cf39a5b5045
Vasily Vasilkov <chand0s@yandex.ru>
2023-07-08 00:11:47 +0400

    Add workflow on pull request

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,12 @@
 pluginManagement {
     val kotlinVersion: String by settings
+    val nexusPublishVersion: String by settings
+    val nebulaReleaseVersion: String by settings
 
     plugins {
-        id("java")
         id("org.jetbrains.kotlin.jvm") version kotlinVersion
+        id("io.github.gradle-nexus.publish-plugin") version nexusPublishVersion
+        id("com.netflix.nebula.release") version nebulaReleaseVersion
     }
 
     repositories {
