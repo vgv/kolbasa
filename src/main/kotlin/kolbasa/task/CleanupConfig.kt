c2b3865c8e0f6d4b1d0d94ccf8e82d041ee130cf
Vasily Vasilkov <chand0s@yandex.ru>
2023-07-16 08:52:43 +0400

    Add cleanup tests

--- src/main/kotlin/kolbasa/task/CleanupConfig.kt
+++ src/main/kotlin/kolbasa/task/CleanupConfig.kt
@@ -4,9 +4,10 @@ import kolbasa.queue.Checks
 import kolbasa.queue.Queue
 
 data class CleanupConfig(
+    val enabled: Boolean = true,
     val cleanupLimit: Int = 1000,
     val cleanupMaxIterations: Int = 10,
-    val cleanupProbabilityPercent: Int = 5,
+    val cleanupProbabilityPercent: Int = 20,
     val cleanupLockIdGenerator: (queue: Queue<*, *>) -> Long = ::defaultLockIdGenerator
 ) {
 
