1a3eb7b73e6b003521ec48ed5c9acb291cef6969
Vasily Vasilkov <chand0s@yandex.ru>
2023-07-16 21:13:36 +0400

    Add comments

--- src/main/kotlin/kolbasa/producer/ProducerOptions.kt
+++ src/main/kotlin/kolbasa/producer/ProducerOptions.kt
@@ -18,8 +18,41 @@ data class ProducerOptions(
     }
 }
 
+/**
+ * Different options how to deal with sending failures
+ *
+ * Imagine you want to send 10,000 messages using a single producer.send() call, but there is one
+ * invalid message in these 10,000 messages which can't be sent. So, there definitely will be a sending failure.
+ * Which options do we have to deal with that failure?
+ *
+ * We have three different options, please read comments below
+ */
 enum class PartialInsert {
+    /**
+     * All 10,000 messages fail
+     */
     PROHIBITED,
+
+    /**
+     * We will send 10,000 messages chunk by chunk (chunk size is ProducerOptions.batchSize). When we encounter
+     * the first chunk with an invalid message (or messages), we stop sending and commit all chunks we sent before.
+     *
+     * For example, batchSize is 1000 and invalid message is 6,500th among 10,000 messages. In this case, we will
+     * first send 6000 messages, then we will meet a chunk with an invalid message and stop.
+     *
+     * So, in total we will send 6,000 messages and not send 4,000.
+     */
     UNTIL_FIRST_FAILURE,
+
+    /**
+     * We will send 10,000 messages chunk by chunk (chunk size is ProducerOptions.batchSize). When we encounter
+     * the first chunk with an invalid message (or messages), we stop sending and commit all chunks we sent before.
+     *
+     * For example, batchSize is 1000 and invalid message is 6,500 among 10,000 messages. In this case, we will
+     * first send 6000 messages, then we will meet a chunk with an invalid message, skip it and continue sending
+     * another 4000 messages.
+     *
+     * So, in total we will send 9,000 messages and not send 1,000.
+     */
     INSERT_AS_MANY_AS_POSSIBLE
 }
