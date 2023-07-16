bfac1a9f8467b1c2bbdb45eb020e49b071df5bc4
Vasily Vasilkov <chand0s@yandex.ru>
2023-07-16 14:29:16 +0400

    Add comments and tests

--- src/main/kotlin/kolbasa/task/CleanupConfig.kt
+++ src/main/kotlin/kolbasa/task/CleanupConfig.kt
@@ -2,6 +2,7 @@ package kolbasa.task
 
 import kolbasa.queue.Checks
 import kolbasa.queue.Queue
+import kolbasa.schema.Const
 
 data class CleanupConfig(
     /**
@@ -12,16 +13,16 @@ data class CleanupConfig(
     /**
      * Max rows to delete at each cleanup iteration
      */
-    val cleanupLimit: Int = 1000,
+    val cleanupLimit: Int = Const.DEFAULT_CLEANUP_ROWS,
     /**
      * Max iterations at each cleanup
      */
-    val cleanupMaxIterations: Int = 10,
+    val cleanupMaxIterations: Int = Const.DEFAULT_CLEANUP_ITERATIONS,
     /**
      * What is the probability that the cleanup will trigger?
      * Default is 20%, so, every fifth consuming will trigger the cleanup process
      */
-    val cleanupProbabilityPercent: Int = 20,
+    val cleanupProbabilityPercent: Int = Const.DEFAULT_CLEANUP_PROBABILITY_PERCENT,
     /**
      * Kolbasa uses PG advisory locks to prevent cleanup one queue by multiple processes at the same time.
      * By default, we use simple lock id generator based on queue name. However, if your code actively uses
