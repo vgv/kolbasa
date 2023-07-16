cf04c4b9da87bb841b8976b2c0a8cb926d3c7548
Vasily Vasilkov <chand0s@yandex.ru>
2023-07-16 09:05:49 +0400

    Add comments

--- src/main/kotlin/kolbasa/task/CleanupConfig.kt
+++ src/main/kotlin/kolbasa/task/CleanupConfig.kt
@@ -4,10 +4,29 @@ import kolbasa.queue.Checks
 import kolbasa.queue.Queue
 
 data class CleanupConfig(
+    /**
+     * Do we need to remove outdated records "in place", i.e. when consuming records?
+     * It is possible to completely disable "in place" cleanup and do it at your own schedule
+     */
     val enabled: Boolean = true,
+    /**
+     * Max rows to delete at each cleanup iteration
+     */
     val cleanupLimit: Int = 1000,
+    /**
+     * Max iterations at each cleanup
+     */
     val cleanupMaxIterations: Int = 10,
+    /**
+     * What is the probability that the cleanup will trigger?
+     * Default is 20%, so, every fifth consuming will trigger the cleanup process
+     */
     val cleanupProbabilityPercent: Int = 20,
+    /**
+     * Kolbasa uses PG advisory locks to prevent cleanup one queue by multiple processes at the same time.
+     * By default, we use simple lock id generator based on queue name. However, if your code actively uses
+     * PG advisory locks, you may need to override this generator and make it more aligned with your code.
+     */
     val cleanupLockIdGenerator: (queue: Queue<*, *>) -> Long = ::defaultLockIdGenerator
 ) {
 
