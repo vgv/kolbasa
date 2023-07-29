c50210d4647e86da3a22a3dc5de4493c0816041a
Vasily Vasilkov <chand0s@yandex.ru>
2023-07-29 11:42:47 +0400

    Remove unused imports

--- src/main/kotlin/kolbasa/stats/task/UpdateRealtimeStatsTask.kt
+++ src/main/kotlin/kolbasa/stats/task/UpdateRealtimeStatsTask.kt
@@ -2,7 +2,6 @@ package kolbasa.stats.task
 
 import kolbasa.Kolbasa
 import kolbasa.stats.GlobalStats
-import org.slf4j.LoggerFactory
 
 internal class UpdateRealtimeStatsTask : AbstractDumpStatsTask() {
 
