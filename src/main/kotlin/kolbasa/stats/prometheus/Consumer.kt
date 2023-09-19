0c6c6f7fe4189b6aa182f31ea7cf45c906b74a6b
Vasily Vasilkov <chand0s@yandex.ru>
2023-09-19 22:07:09 +0400

    Add prometheus to consumer

---
+++ src/main/kotlin/kolbasa/stats/prometheus/Consumer.kt
@@ -0,0 +1,67 @@
+package kolbasa.stats.prometheus
+
+import io.prometheus.client.Counter
+import io.prometheus.client.Histogram
+
+internal object Consumer {
+
+    val consumerReceiveCounter = Counter.Builder()
+        .namespace("kolbasa")
+        .name("consumer_receive")
+        .help("Amount of consumer receive() calls")
+        .labelNames("queue")
+        .create()
+        .register<Counter>()
+
+    val consumerReceiveBytesCounter = Counter.Builder()
+        .namespace("kolbasa")
+        .name("consumer_receive_bytes")
+        .help("Amount of bytes read by consumer receive() calls")
+        .labelNames("queue")
+        .create()
+        .register<Counter>()
+
+    val consumerReceiveRowsCounter = Counter.Builder()
+        .namespace("kolbasa")
+        .name("consumer_receive_rows")
+        .help("Amount of rows received by consumer receive() calls")
+        .labelNames("queue")
+        .create()
+        .register<Counter>()
+
+    val consumerReceiveDuration = Histogram.Builder()
+        .namespace("kolbasa")
+        .name("consumer_receive_duration_seconds")
+        .help("Consumer receive() calls duration")
+        .labelNames("queue")
+        .buckets(*Const.histogramBuckets())
+        .create()
+        .register<Histogram>()
+
+
+    val consumerDeleteCounter = Counter.Builder()
+        .namespace("kolbasa")
+        .name("consumer_delete")
+        .help("Amount of consumer delete() calls")
+        .labelNames("queue")
+        .create()
+        .register<Counter>()
+
+    val consumerDeleteRowsCounter = Counter.Builder()
+        .namespace("kolbasa")
+        .name("consumer_delete_rows")
+        .help("Amount of rows removed by consumer delete() calls")
+        .labelNames("queue")
+        .create()
+        .register<Counter>()
+
+    val consumerDeleteDuration = Histogram.Builder()
+        .namespace("kolbasa")
+        .name("consumer_delete_duration_seconds")
+        .help("Consumer delete() calls duration")
+        .labelNames("queue")
+        .buckets(*Const.histogramBuckets())
+        .create()
+        .register<Histogram>()
+
+}
