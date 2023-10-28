2db59c3eedfb71b746274f0fc1a6e8f05da561a4
Vasily Vasilkov <chand0s@yandex.ru>
2023-10-28 15:53:31 +0400

    Add comments to ReceiveOptions

--- src/main/kotlin/kolbasa/consumer/ReceiveOptions.kt
+++ src/main/kotlin/kolbasa/consumer/ReceiveOptions.kt
@@ -7,10 +7,25 @@ import kolbasa.consumer.order.Order
 import java.time.Duration
 
 data class ReceiveOptions<Meta : Any> @JvmOverloads constructor(
+    /**
+     * Visibility timeout for this specific consume() call.
+     * Visibility timeout is a delay before consumed but not deleted message will be
+     * visible to another consumers. By default, value is not set, which means the
+     * [ConsumerOptions.visibilityTimeout][kolbasa.consumer.ConsumerOptions.visibilityTimeout] will be used or,
+     * if it's not set too, the
+     * [QueueOptions.defaultVisibilityTimeout][kolbasa.queue.QueueOptions.defaultVisibilityTimeout] will be used.
+     *
+     * Has the highest priority, next is
+     * [ConsumerOptions.visibilityTimeout][kolbasa.consumer.ConsumerOptions.visibilityTimeout] and, at the end,
+     * [QueueOptions.defaultVisibilityTimeout][kolbasa.queue.QueueOptions.defaultVisibilityTimeout]
+     */
     val visibilityTimeout: Duration = QueueOptions.VISIBILITY_TIMEOUT_NOT_SET,
     /**
      * Do we need to read metadata?
-     * By default, we don't want to read these fields from the DB, parse them and instantiate the meta-class Meta
+     * By default, we don't want to read these fields from the DB, parse them and instantiate the
+     * meta-class [Meta] for performance reasons.
+     *
+     * You don't need to read metadata if the only thing you need is to filter (or sort) messages by metadata fields.
      */
     val readMetadata: Boolean = false,
     val order: List<Order<Meta>>? = null,
