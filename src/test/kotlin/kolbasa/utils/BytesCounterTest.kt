2af57e6a9cbae4d6b5293b9408c3ede4bf3cc88b
Vasily Vasilkov <chand0s@yandex.ru>
2023-10-29 13:48:47 +0400

    Remove LongBox, create special class for calculating amount of bytes

---
+++ src/test/kotlin/kolbasa/utils/BytesCounterTest.kt
@@ -0,0 +1,19 @@
+package kolbasa.utils
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class BytesCounterTest {
+
+    @Test
+    fun testLongBox() {
+        val box = BytesCounter()
+
+        assertEquals(0, box.get())
+        box.inc(5)
+        assertEquals(0 + 5, box.get())
+        box.inc(42)
+        assertEquals(0 + 5 + 42, box.get())
+    }
+
+}
