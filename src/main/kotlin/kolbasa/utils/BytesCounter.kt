fc24614be8d1a89e219723e909578c1f92a1c327
Vasily Vasilkov <chand0s@yandex.ru>
2023-10-30 20:42:22 +0400

    Add precise way to calculate string size for Prometheus

--- src/main/kotlin/kolbasa/utils/BytesCounter.kt
+++ src/main/kotlin/kolbasa/utils/BytesCounter.kt
@@ -1,12 +1,35 @@
 package kolbasa.utils
 
-internal class BytesCounter {
+/**
+ * Counts bytes of data that will be sent or received from the database.
+ *
+ * @param precise if true, then the size of the string will be calculated in
+ * bytes (much slower, but precise), otherwise in characters (very fast, but not so precise)
+ */
+internal class BytesCounter(private val precise: Boolean) {
 
     private var bytes: Long = 0
 
     fun get(): Long = bytes
 
-    fun inc(delta: Int) {
-        bytes += delta
+    fun addInt() {
+        bytes += 4
     }
+
+    fun addLong() {
+        bytes += 8
+    }
+
+    fun addString(value: String) {
+        bytes += if (precise) {
+            value.toByteArray().size
+        } else {
+            value.length
+        }
+    }
+
+    fun addByteArray(value: ByteArray) {
+        bytes += value.size
+    }
+
 }
