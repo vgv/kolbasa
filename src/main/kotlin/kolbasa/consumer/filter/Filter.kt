4c55194c951d3616100267bef9bb2c4cd4d9e602
Vasily Vasilkov <chand0s@yandex.ru>
2023-11-04 16:41:21 +0400

    Add comments

--- src/main/kotlin/kolbasa/consumer/filter/Filter.kt
+++ src/main/kotlin/kolbasa/consumer/filter/Filter.kt
@@ -6,84 +6,246 @@ import kotlin.reflect.KProperty1
 
 object Filter {
 
+    /**
+     * PostgreSQL normal equality operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Meta::field eq 42
+     * ```
+     * means `meta_field = 42`
+     */
     infix fun <Meta : Any, T : Comparable<T>> KProperty1<Meta, T?>.eq(value: T): Condition<Meta> {
         return EqCondition(this.name, value)
     }
 
+    /**
+     * PostgreSQL normal equality operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Meta::field eq 42
+     * ```
+     * means `meta_field = 42`
+     */
     infix fun <Meta : Any, T : Comparable<T>> KFunction1<Meta, T?>.eq(value: T): Condition<Meta> {
         return EqCondition(this.name, value)
     }
 
+    /**
+     * PostgreSQL normal equality operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Filter.eq(JavaField.of(...), 42)
+     * ```
+     * means `meta_field = 42`
+     */
     @JvmStatic
     infix fun <Meta : Any, T : Comparable<T>> JavaField<Meta, T?>.eq(value: T): Condition<Meta> {
         return EqCondition(this.name, value)
     }
 
     // -------------------------------------------------------------------------------------------
+    /**
+     * PostgreSQL normal 'not equal' operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Meta::field neq 42
+     * ```
+     * means `meta_field <> 42`
+     */
     infix fun <Meta : Any, T : Comparable<T>> KProperty1<Meta, T?>.neq(value: T): Condition<Meta> {
         return NeqCondition(this.name, value)
     }
 
+    /**
+     * PostgreSQL normal 'not equal' operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Meta::field neq 42
+     * ```
+     * means `meta_field <> 42`
+     */
     infix fun <Meta : Any, T : Comparable<T>> KFunction1<Meta, T?>.neq(value: T): Condition<Meta> {
         return NeqCondition(this.name, value)
     }
 
+    /**
+     * PostgreSQL normal 'not equal' operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Filter.neq(JavaField.of(...), 42)
+     * ```
+     * means `meta_field <> 42`
+     */
     @JvmStatic
     infix fun <Meta : Any, T : Comparable<T>> JavaField<Meta, T?>.neq(value: T): Condition<Meta> {
         return NeqCondition(this.name, value)
     }
 
     // -------------------------------------------------------------------------------------------
+    /**
+     * PostgreSQL 'greater than' operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Meta::field greater 42
+     * ```
+     * means `meta_field > 42`
+     */
     infix fun <Meta : Any, T : Comparable<T>> KProperty1<Meta, T?>.greater(value: T): Condition<Meta> {
         return GreaterThanCondition(this.name, value)
     }
 
+    /**
+     * PostgreSQL 'greater than' operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Meta::field greater 42
+     * ```
+     * means `meta_field > 42`
+     */
     infix fun <Meta : Any, T : Comparable<T>> KFunction1<Meta, T?>.greater(value: T): Condition<Meta> {
         return GreaterThanCondition(this.name, value)
     }
 
+    /**
+     * PostgreSQL 'greater than' operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Filter.greater(JavaField.of(...), 42)
+     * ```
+     * means `meta_field > 42`
+     */
     @JvmStatic
     infix fun <Meta : Any, T : Comparable<T>> JavaField<Meta, T?>.greater(value: T): Condition<Meta> {
         return GreaterThanCondition(this.name, value)
     }
 
     // -------------------------------------------------------------------------------------------
+    /**
+     * PostgreSQL 'greater than or equal to' operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Meta::field greaterEq 42
+     * ```
+     * means `meta_field >= 42`
+     */
     infix fun <Meta : Any, T : Comparable<T>> KProperty1<Meta, T?>.greaterEq(value: T): Condition<Meta> {
         return GreaterThanOrEqCondition(this.name, value)
     }
 
+    /**
+     * PostgreSQL 'greater than or equal to' operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Meta::field greaterEq 42
+     * ```
+     * means `meta_field >= 42`
+     */
     infix fun <Meta : Any, T : Comparable<T>> KFunction1<Meta, T?>.greaterEq(value: T): Condition<Meta> {
         return GreaterThanOrEqCondition(this.name, value)
     }
 
+    /**
+     * PostgreSQL 'greater than or equal to' operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Filter.greaterEq(JavaField.of(...), 42)
+     * ```
+     * means `meta_field >= 42`
+     */
     @JvmStatic
     infix fun <Meta : Any, T : Comparable<T>> JavaField<Meta, T?>.greaterEq(value: T): Condition<Meta> {
         return GreaterThanOrEqCondition(this.name, value)
     }
 
     // -------------------------------------------------------------------------------------------
+    /**
+     * PostgreSQL 'less than' operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Meta::field less 42
+     * ```
+     * means `meta_field < 42`
+     */
     infix fun <Meta : Any, T : Comparable<T>> KProperty1<Meta, T?>.less(value: T): Condition<Meta> {
         return LessThanCondition(this.name, value)
     }
 
+    /**
+     * PostgreSQL 'less than' operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Meta::field less 42
+     * ```
+     * means `meta_field < 42`
+     */
     infix fun <Meta : Any, T : Comparable<T>> KFunction1<Meta, T?>.less(value: T): Condition<Meta> {
         return LessThanCondition(this.name, value)
     }
 
+    /**
+     * PostgreSQL 'less than' operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Filter.less(JavaField.of(...), 42)
+     * ```
+     * means `meta_field < 42`
+     */
     @JvmStatic
     infix fun <Meta : Any, T : Comparable<T>> JavaField<Meta, T?>.less(value: T): Condition<Meta> {
         return LessThanCondition(this.name, value)
     }
 
     // -------------------------------------------------------------------------------------------
+    /**
+     * PostgreSQL 'less than or equal to' operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Meta::field lessEq 42
+     * ```
+     * means `meta_field <= 42`
+     */
     infix fun <Meta : Any, T : Comparable<T>> KProperty1<Meta, T?>.lessEq(value: T): Condition<Meta> {
         return LessThanOrEqCondition(this.name, value)
     }
 
+    /**
+     * PostgreSQL 'less than or equal to' operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Meta::field lessEq 42
+     * ```
+     * means `meta_field <= 42`
+     */
     infix fun <Meta : Any, T : Comparable<T>> KFunction1<Meta, T?>.lessEq(value: T): Condition<Meta> {
         return LessThanOrEqCondition(this.name, value)
     }
 
+    /**
+     * PostgreSQL 'less than or equal to' operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Filter.lessEq(JavaField.of(...), 42)
+     * ```
+     * means `meta_field <= 42`
+     */
     @JvmStatic
     infix fun <Meta : Any, T : Comparable<T>> JavaField<Meta, T?>.lessEq(value: T): Condition<Meta> {
         return LessThanOrEqCondition(this.name, value)
@@ -91,36 +253,45 @@ object Filter {
 
     // -------------------------------------------------------------------------------------------
     /**
-     * Both values are inclusive, the same as `between` in SQL.
+     * PostgreSQL 'between' operator.
      *
+     * Usage is the same as in SQL:
      * ```
-     * field between Pair(10, 20)
+     * Meta::field between Pair(10, 20)
      * ```
-     * means (field >= 10 and field <= 20)
+     * means `(meta_field between 10 and 20)`
+     *
+     * Both values are inclusive, the same as `between` in SQL.
      */
     infix fun <Meta : Any, T : Comparable<T>> KProperty1<Meta, T?>.between(value: Pair<T, T>): Condition<Meta> {
         return BetweenCondition(this.name, value)
     }
 
     /**
-     * Both values are inclusive, the same as `between` in SQL.
+     * PostgreSQL 'between' operator.
      *
+     * Usage is the same as in SQL:
      * ```
-     * field between Pair(10, 20)
+     * Meta::field between Pair(10, 20)
      * ```
-     * means (field >= 10 and field <= 20)
+     * means `(meta_field between 10 and 20)`
+     *
+     * Both values are inclusive, the same as `between` in SQL.
      */
     infix fun <Meta : Any, T : Comparable<T>> KFunction1<Meta, T?>.between(value: Pair<T, T>): Condition<Meta> {
         return BetweenCondition(this.name, value)
     }
 
     /**
-     * Both values are inclusive, the same as `between` in SQL.
+     * PostgreSQL 'between' operator.
      *
+     * Usage is the same as in SQL:
      * ```
-     * field between Pair(10, 20)
+     * Filter.between(JavaField.of(...), new Pair<>(10, 20))
      * ```
-     * means (field >= 10 and field <= 20)
+     * means `(meta_field between 10 and 20)`
+     *
+     * Both values are inclusive, the same as `between` in SQL.
      */
     @JvmStatic
     infix fun <Meta : Any, T : Comparable<T>> JavaField<Meta, T?>.between(value: Pair<T, T>): Condition<Meta> {
@@ -128,14 +299,41 @@ object Filter {
     }
 
     // -------------------------------------------------------------------------------------------
+    /**
+     * PostgreSQL classic like operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Meta::field like "abc%"
+     * ```
+     * means `meta_field like 'abc%'`
+     */
     infix fun <Meta : Any> KProperty1<Meta, String?>.like(value: String): Condition<Meta> {
         return LikeCondition(this.name, value)
     }
 
+    /**
+     * PostgreSQL classic like operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Meta::field like "abc%"
+     * ```
+     * means `meta_field like 'abc%'`
+     */
     infix fun <Meta : Any> KFunction1<Meta, String?>.like(value: String): Condition<Meta> {
         return LikeCondition(this.name, value)
     }
 
+    /**
+     * PostgreSQL classic like operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Filter.like(JavaField.of(...), "abc%")
+     * ```
+     * means `meta_field like 'abc%'`
+     */
     @JvmStatic
     infix fun <Meta : Any> JavaField<Meta, String?>.like(value: String): Condition<Meta> {
         return LikeCondition(this.name, value)
@@ -154,28 +352,82 @@ object Filter {
     }
 
     // -------------------------------------------------------------------------------------------
+    /**
+     * PostgreSQL 'is null' operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * isNull(Meta::field)
+     * ```
+     * means `meta_field is null`
+     */
     fun <Meta : Any, T : Comparable<T>> isNull(property: KProperty1<Meta, T?>): Condition<Meta> {
         return IsNullCondition(property.name)
     }
 
+    /**
+     * PostgreSQL 'is null' operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * isNull(Meta::field)
+     * ```
+     * means `meta_field is null`
+     */
     fun <Meta : Any, T : Comparable<T>> isNull(property: KFunction1<Meta, T?>): Condition<Meta> {
         return IsNullCondition(property.name)
     }
 
+    /**
+     * PostgreSQL 'is null' operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Filter.isNull(JavaField.of(...))
+     * ```
+     * means `meta_field is null`
+     */
     @JvmStatic
     fun <Meta : Any, T : Comparable<T>> isNull(property: JavaField<Meta, T?>): Condition<Meta> {
         return IsNullCondition(property.name)
     }
 
     // -------------------------------------------------------------------------------------------
+    /**
+     * PostgreSQL 'is not null' operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * isNotNull(Meta::field)
+     * ```
+     * means `meta_field is not null`
+     */
     fun <Meta : Any, T : Comparable<T>> isNotNull(property: KProperty1<Meta, T?>): Condition<Meta> {
         return IsNotNullCondition(property.name)
     }
 
+    /**
+     * PostgreSQL 'is not null' operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * isNotNull(Meta::field)
+     * ```
+     * means `meta_field is not null`
+     */
     fun <Meta : Any, T : Comparable<T>> isNotNull(property: KFunction1<Meta, T?>): Condition<Meta> {
         return IsNotNullCondition(property.name)
     }
 
+    /**
+     * PostgreSQL 'is not null' operator.
+     *
+     * Usage is the same as in SQL:
+     * ```
+     * Filter.isNotNull(JavaField.of(...))
+     * ```
+     * means `meta_field is not null`
+     */
     @JvmStatic
     fun <Meta : Any, T : Comparable<T>> isNotNull(property: JavaField<Meta, T?>): Condition<Meta> {
         return IsNotNullCondition(property.name)
@@ -240,7 +492,7 @@ object Filter {
      *
      * Usage
      * ```
-     * nativeSql("sin({0}) > 0.7 and {1}*{1}>1000", MyMeta::field1, MyMeta::field2)
+     * nativeSql("sin({0}) > 0.7 and {1}*{1}>1000", JavaField.of(...), JavaField.of(...))
      * ```
      *
      * This expression will be converted into this SQL expression:
