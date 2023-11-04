b3cf0ee91ce917cfe7b8a95e712ef82367f330cb
Vasily Vasilkov <chand0s@yandex.ru>
2023-11-04 18:28:16 +0400

    Add more helper methods

--- src/main/kotlin/kolbasa/schema/SchemaHelpers.kt
+++ src/main/kotlin/kolbasa/schema/SchemaHelpers.kt
@@ -9,7 +9,7 @@ object SchemaHelpers {
      * Generate all statements needed to create/update database schema, but doesn't execute them
      */
     @JvmStatic
-    fun generateDatabaseSchema(dataSource: DataSource, vararg queues: Queue<*, *>): Map<Queue<*, *>, Schema> {
+    fun generateDatabaseSchema(dataSource: DataSource, queues: List<Queue<*, *>>): Map<Queue<*, *>, Schema> {
         val existingTables = SchemaExtractor.extractRawSchema(dataSource, tableNamePattern = null)
 
         return queues.associateWith { queue ->
@@ -19,14 +19,30 @@ object SchemaHelpers {
     }
 
     /**
+     * Generate all statements needed to create/update database schema, but doesn't execute them
+     */
+    @JvmStatic
+    fun generateDatabaseSchema(dataSource: DataSource, vararg queues: Queue<*, *>): Map<Queue<*, *>, Schema> {
+        return generateDatabaseSchema(dataSource, queues.toList())
+    }
+
+    /**
+     * Update database schema
+     */
+    @JvmStatic
+    fun updateDatabaseSchema(dataSource: DataSource, queues: List<Queue<*, *>>) {
+        generateDatabaseSchema(dataSource, queues).forEach { (_, schema) ->
+            // we execute only required statements
+            executeSchemaStatements(dataSource, schema.required)
+        }
+    }
+
+    /**
      * Update database schema
      */
     @JvmStatic
     fun updateDatabaseSchema(dataSource: DataSource, vararg queues: Queue<*, *>) {
-        generateDatabaseSchema(dataSource, *queues).forEach { (_, schema) ->
-            // we execute only required statements
-            executeSchemaStatements(dataSource, schema.required)
-        }
+        updateDatabaseSchema(dataSource, queues.toList())
     }
 
     private fun executeSchemaStatements(dataSource: DataSource, statements: SchemaStatements) {
