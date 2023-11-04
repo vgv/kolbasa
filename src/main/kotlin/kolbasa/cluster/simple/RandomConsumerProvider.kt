e75ead3cf651208d0431f86af8762988f60d2d17
Vasily Vasilkov <chand0s@yandex.ru>
2023-11-04 21:22:09 +0400

    Rework cluster providers to make it easier to use

--- src/main/kotlin/kolbasa/cluster/simple/RandomConsumerProvider.kt
+++ src/main/kotlin/kolbasa/cluster/simple/RandomConsumerProvider.kt
@@ -4,20 +4,35 @@ import kolbasa.consumer.Consumer
 import kolbasa.consumer.ConsumerOptions
 import kolbasa.consumer.DatabaseConsumer
 import kolbasa.queue.Queue
+import java.util.concurrent.ConcurrentHashMap
 import javax.sql.DataSource
 
-class RandomConsumerProvider<Data, Meta : Any>(
-    dataSources: List<DataSource>,
-    queue: Queue<Data, Meta>,
-    consumerOptions: ConsumerOptions = ConsumerOptions()
-) : ConsumerProvider<Data, Meta> {
+class RandomConsumerProvider(
+    private val dataSources: List<DataSource>,
+    private val consumerOptions: ConsumerOptions = ConsumerOptions()
+) : ConsumerProvider {
 
-    private val consumers = dataSources.map { dataSource ->
-        DatabaseConsumer(dataSource, queue, consumerOptions)
+    private val consumers: MutableMap<Queue<*, *>, List<Consumer<*, *>>> = ConcurrentHashMap()
+
+    init {
+        check(dataSources.isNotEmpty()) {
+            "You have to provide at least one DataSource"
+        }
     }
 
-    override fun consumer(): Consumer<Data, Meta> {
-        return consumers.random()
+    override fun <Data, Meta : Any> consumer(queue: Queue<Data, Meta>): Consumer<Data, Meta> {
+        val queueConsumers = consumers.computeIfAbsent(queue) {
+            generateConsumers(queue)
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        return queueConsumers.random() as Consumer<Data, Meta>
+    }
+
+    private fun <Data, Meta : Any> generateConsumers(queue: Queue<Data, Meta>): List<Consumer<Data, Meta>> {
+        return dataSources.map { dataSource ->
+            DatabaseConsumer(dataSource, queue, consumerOptions)
+        }
     }
 
 }
