4ac7537e696d8e56c95ee1d1525a48664f5bffc7
Vasily Vasilkov <chand0s@yandex.ru>
2023-11-18 13:38:49 +0400

    Fix default value for scheduled_at

--- src/main/kotlin/kolbasa/schema/SchemaGenerator.kt
+++ src/main/kotlin/kolbasa/schema/SchemaGenerator.kt
@@ -61,7 +61,7 @@ internal object SchemaGenerator {
             val alterStatement = """
                     alter table ${queue.dbTableName}
                     alter ${Const.SCHEDULED_AT_COLUMN_NAME}
-                    drop default
+                    set default clock_timestamp()
                 """.trimIndent()
 
             if (existingTable != null) {
