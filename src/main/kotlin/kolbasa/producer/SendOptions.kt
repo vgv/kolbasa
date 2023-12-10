7fc1bc5628bc2c8154128cb6fb193acfe8dc7f39
Vasily Vasilkov <chand0s@yandex.ru>
2023-12-10 17:58:28 +0400

    Add builder to SendOptions

--- src/main/kotlin/kolbasa/producer/SendOptions.kt
+++ src/main/kotlin/kolbasa/producer/SendOptions.kt
@@ -23,10 +23,23 @@ data class SendOptions(
         Checks.checkBatchSize(batchSize)
     }
 
-    internal companion object {
+    class Builder internal constructor() {
+        private var deduplicationMode: DeduplicationMode = DeduplicationMode.ERROR
+        private var batchSize: Int = ProducerOptions.DEFAULT_BATCH_SIZE
+        private var partialInsert: PartialInsert = PartialInsert.PROHIBITED
 
+        fun deduplicationMode(deduplicationMode: DeduplicationMode) = apply { this.deduplicationMode = deduplicationMode }
+        fun batchSize(batchSize: Int) = apply { this.batchSize = batchSize }
+        fun partialInsert(partialInsert: PartialInsert) = apply { this.partialInsert = partialInsert }
+
+        fun build() = SendOptions(deduplicationMode, batchSize, partialInsert)
+    }
+
+    internal companion object {
         val SEND_OPTIONS_NOT_SET = SendOptions()
 
+        @JvmStatic
+        fun builder() = Builder()
     }
 
 }
