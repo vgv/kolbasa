fe691fe4757a7b16ea88463bee9920112e662bf0
Vasily Vasilkov <chand0s@yandex.ru>
2024-06-08 23:12:22 +0400

    Add consumer interceptors

---
+++ src/main/kotlin/kolbasa/consumer/connection/ConnectionAwareConsumerInterceptor.kt
@@ -0,0 +1,103 @@
+package kolbasa.consumer.connection
+
+import kolbasa.consumer.Message
+import kolbasa.consumer.ReceiveOptions
+import java.sql.Connection
+
+interface ConnectionAwareConsumerInterceptor<Data, Meta : Any> {
+
+    fun beforeReceive(
+        connection: Connection,
+        limit: Int,
+        receiveOptions: ReceiveOptions<Meta>
+    ): ReceiveOptions<Meta> {
+        return receiveOptions
+    }
+
+    fun aroundReceive(
+        connection: Connection,
+        limit: Int,
+        receiveOptions: ReceiveOptions<Meta>,
+        call: (Connection, Int, ReceiveOptions<Meta>) -> List<Message<Data, Meta>>
+    ): List<Message<Data, Meta>> {
+        return call(connection, limit, receiveOptions)
+    }
+
+    fun afterReceive(
+        connection: Connection,
+        result: List<Message<Data, Meta>>
+    ): List<Message<Data, Meta>> {
+        return result
+    }
+
+    fun beforeDelete(
+        connection: Connection,
+        messageIds: List<Long>
+    ): List<Long> {
+        return messageIds
+    }
+
+    fun aroundDelete(
+        connection: Connection,
+        messageIds: List<Long>,
+        call: (Connection, List<Long>) -> Int
+    ): Int {
+        return call(connection, messageIds)
+    }
+
+    fun afterDelete(
+        connection: Connection,
+        result: Int
+    ): Int {
+        return result
+    }
+
+
+    companion object {
+
+        internal tailrec fun <Data, Meta : Any> recursiveApplyReceiveInterceptors(
+            interceptors: List<ConnectionAwareConsumerInterceptor<Data, Meta>>,
+            connection: Connection,
+            limit: Int,
+            receiveOptions: ReceiveOptions<Meta>,
+            call: (Connection, Int, ReceiveOptions<Meta>) -> List<Message<Data, Meta>>
+        ): List<Message<Data, Meta>> {
+            if (interceptors.isEmpty()) {
+                return call(connection, limit, receiveOptions)
+            } else {
+                val topInterceptors = interceptors.subList(0, interceptors.size - 1)
+                val deepestInterceptor = interceptors.last()
+                return recursiveApplyReceiveInterceptors(
+                    topInterceptors,
+                    connection,
+                    limit,
+                    receiveOptions
+                ) { conn, lim, rcvOptions ->
+                    val interceptedReceiveOptions = deepestInterceptor.beforeReceive(conn, lim, rcvOptions)
+                    val result = deepestInterceptor.aroundReceive(conn, lim, interceptedReceiveOptions, call)
+                    deepestInterceptor.afterReceive(conn, result)
+                }
+            }
+        }
+
+        internal tailrec fun <Data, Meta : Any> recursiveApplyDeleteInterceptors(
+            interceptors: List<ConnectionAwareConsumerInterceptor<Data, Meta>>,
+            connection: Connection,
+            messageIds: List<Long>,
+            call: (Connection, List<Long>) -> Int
+        ): Int {
+            if (interceptors.isEmpty()) {
+                return call(connection, messageIds)
+            } else {
+                val topInterceptors = interceptors.subList(0, interceptors.size - 1)
+                val deepestInterceptor = interceptors.last()
+                return recursiveApplyDeleteInterceptors(topInterceptors, connection, messageIds) { conn, msgIds ->
+                    val interceptedMessageIds = deepestInterceptor.beforeDelete(conn, msgIds)
+                    val result = deepestInterceptor.aroundDelete(conn, interceptedMessageIds, call)
+                    deepestInterceptor.afterDelete(conn, result)
+                }
+            }
+        }
+    }
+
+}
