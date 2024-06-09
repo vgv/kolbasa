53f35517aaf42f60318e85c2a386d987c392aac7
Vasily Vasilkov <chand0s@yandex.ru>
2024-06-09 22:04:48 +0400

    Test commit, try to remove hard io.prometheus.* dependency

--- src/main/kotlin/kolbasa/stats/prometheus/PrometheusConfig.kt
+++ src/main/kotlin/kolbasa/stats/prometheus/PrometheusConfig.kt
@@ -5,73 +5,79 @@ import kolbasa.queue.Checks
 import kolbasa.queue.Queue
 import java.time.Duration
 
-data class PrometheusConfig(
-    /**
-     * Collect Prometheus metrics or not.
-     */
-    val enabled: Boolean = false,
+sealed class PrometheusConfig {
 
-    /**
-     * By default, the size of the string to send to Prometheus is calculated in characters, not in bytes, which is much
-     * faster. If you really need to calculate exact string size in bytes, set this option to true (but it's much slower).
-     */
-    val preciseStringSize: Boolean = false,
+    object None: PrometheusConfig()
 
-    /**
-     * By default, kolbasa measures the queue size every [PrometheusConfig.DEFAULT_QUEUE_SIZE_MEASURE_INTERVAL],
-     * however, if you want to measure more/less often for specific queues – you can
-     * change the default measurement interval here.
-     *
-     * Please note that current implementation relies on PostgreSQL vacuum statistics, so, if you don't run vacuum every
-     * five seconds (for example) it's almost nonsense to measure the queue size every five seconds.
-     *
-     * Map key: Queue name
-     * Map value: Interval
-     */
-    val customQueueSizeMeasureInterval: Map<String, Duration> = emptyMap(),
+    data class Real(
+        /**
+         * Collect Prometheus metrics or not.
+         */
+        val enabled: Boolean = false,
 
-    /**
-     * Prometheus registry. By default, [PrometheusRegistry.defaultRegistry] is used.
-     * You can use your custom registry, but you need to change the registry before any
-     * other queue operations (at application startup, for example).
-     */
-    val registry: PrometheusRegistry = PrometheusRegistry.defaultRegistry
-) {
+        /**
+         * By default, the size of the string to send to Prometheus is calculated in characters, not in bytes, which is much
+         * faster. If you really need to calculate exact string size in bytes, set this option to true (but it's much slower).
+         */
+        val preciseStringSize: Boolean = false,
 
-    init {
-        customQueueSizeMeasureInterval.forEach { (queueName, customInterval) ->
-            Checks.checkCustomQueueSizeMeasureInterval(queueName, customInterval)
-        }
-    }
+        /**
+         * By default, kolbasa measures the queue size every [PrometheusConfig.DEFAULT_QUEUE_SIZE_MEASURE_INTERVAL],
+         * however, if you want to measure more/less often for specific queues – you can
+         * change the default measurement interval here.
+         *
+         * Please note that current implementation relies on PostgreSQL vacuum statistics, so, if you don't run vacuum every
+         * five seconds (for example) it's almost nonsense to measure the queue size every five seconds.
+         *
+         * Map key: Queue name
+         * Map value: Interval
+         */
+        val customQueueSizeMeasureInterval: Map<String, Duration> = emptyMap(),
 
-    class Builder internal constructor() {
-        private var enabled: Boolean = false
-        private var preciseStringSize: Boolean = false
-        private var customQueueSizeMeasureInterval: MutableMap<String, Duration> = mutableMapOf()
-        private var registry: PrometheusRegistry = PrometheusRegistry.defaultRegistry
+        /**
+         * Prometheus registry. By default, [PrometheusRegistry.defaultRegistry] is used.
+         * You can use your custom registry, but you need to change the registry before any
+         * other queue operations (at application startup, for example).
+         */
+        val registry: PrometheusRegistry = PrometheusRegistry.defaultRegistry
+    ): PrometheusConfig()  {
 
-        fun enabled() = apply { this.enabled = true }
-        fun disabled() = apply { this.enabled = false }
-        fun customQueueSizeMeasureInterval(queueName: String, customInterval: Duration) = apply {
-            customQueueSizeMeasureInterval[queueName] = customInterval
+        init {
+            customQueueSizeMeasureInterval.forEach { (queueName, customInterval) ->
+                Checks.checkCustomQueueSizeMeasureInterval(queueName, customInterval)
+            }
         }
 
-        fun customQueueSizeMeasureInterval(queue: Queue<*, *>, customInterval: Duration) =
-            customQueueSizeMeasureInterval(queue.name, customInterval)
+        class Builder internal constructor() {
+            private var enabled: Boolean = false
+            private var preciseStringSize: Boolean = false
+            private var customQueueSizeMeasureInterval: MutableMap<String, Duration> = mutableMapOf()
+            private var registry: PrometheusRegistry = PrometheusRegistry.defaultRegistry
 
-        fun preciseStringSize(preciseStringSize: Boolean) = apply { this.preciseStringSize = preciseStringSize }
-        fun registry(registry: PrometheusRegistry) = apply { this.registry = registry }
+            fun enabled() = apply { this.enabled = true }
+            fun disabled() = apply { this.enabled = false }
+            fun customQueueSizeMeasureInterval(queueName: String, customInterval: Duration) = apply {
+                customQueueSizeMeasureInterval[queueName] = customInterval
+            }
 
-        fun build() = PrometheusConfig(enabled, preciseStringSize, customQueueSizeMeasureInterval, registry)
+            fun customQueueSizeMeasureInterval(queue: Queue<*, *>, customInterval: Duration) =
+                customQueueSizeMeasureInterval(queue.name, customInterval)
+
+            fun preciseStringSize(preciseStringSize: Boolean) = apply { this.preciseStringSize = preciseStringSize }
+            fun registry(registry: PrometheusRegistry) = apply { this.registry = registry }
+
+            fun build() = Real(enabled, preciseStringSize, customQueueSizeMeasureInterval, registry)
+        }
+
+        companion object {
+
+            internal val MIN_QUEUE_SIZE_MEASURE_INTERVAL = Duration.ofSeconds(1)
+            internal val DEFAULT_QUEUE_SIZE_MEASURE_INTERVAL = Duration.ofSeconds(15)
+
+            @JvmStatic
+            fun builder(): Builder = Builder()
+        }
+
     }
-
-    companion object {
-
-        internal val MIN_QUEUE_SIZE_MEASURE_INTERVAL = Duration.ofSeconds(1)
-        internal val DEFAULT_QUEUE_SIZE_MEASURE_INTERVAL = Duration.ofSeconds(15)
-
-        @JvmStatic
-        fun builder(): Builder = Builder()
-    }
-
 }
+
