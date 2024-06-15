cfed1ecb5697d778338af72b0c2df60e429d610b
Aleksandr Turchenko <turchenko.alexander@gmail.com>
2024-06-15 10:43:26 +0400

    Migrate semconv dependencies from old versions 1.xx to 2.xx
    Removed io.opentelemetry.instrumentation:opentelemetry-instrumentation-api-semconv
    
    Replaced with
    io.opentelemetry.semconv:opentelemetry-semconv-incubating
    io.opentelemetry.instrumentation:opentelemetry-instrumentation-api-incubator

--- src/main/kotlin/kolbasa/stats/opentelemetry/SendRequestAttributesGetter.kt
+++ src/main/kotlin/kolbasa/stats/opentelemetry/SendRequestAttributesGetter.kt
@@ -2,9 +2,9 @@ package kolbasa.stats.opentelemetry
 
 import io.opentelemetry.api.common.AttributesBuilder
 import io.opentelemetry.context.Context
+import io.opentelemetry.instrumentation.api.incubator.semconv.messaging.MessagingAttributesGetter
 import io.opentelemetry.instrumentation.api.instrumenter.AttributesExtractor
-import io.opentelemetry.instrumentation.api.instrumenter.messaging.MessagingAttributesGetter
-import io.opentelemetry.semconv.SemanticAttributes
+import io.opentelemetry.semconv.incubating.MessagingIncubatingAttributes
 import kolbasa.consumer.Message
 import kolbasa.producer.SendRequest
 import kolbasa.producer.SendResult
@@ -28,20 +28,36 @@ internal class SendRequestAttributesGetter<Data, Meta : Any>(private val queueNa
         return null
     }
 
-    override fun getMessagePayloadSize(request: SendRequest<Data, Meta>?): Long? {
-        return null
-    }
-
-    override fun getMessagePayloadCompressedSize(request: SendRequest<Data, Meta>?): Long? {
-        return null
-    }
-
     override fun getMessageId(request: SendRequest<Data, Meta>?, response: SendResult<Data, Meta>?): String? {
         return null
     }
 
     override fun getMessageHeader(request: SendRequest<Data, Meta>?, name: String?): MutableList<String> {
         return super.getMessageHeader(request, name)
+    }
+
+    override fun getDestinationTemplate(request: SendRequest<Data, Meta>?): String? {
+        return null
+    }
+
+    override fun isAnonymousDestination(request: SendRequest<Data, Meta>?): Boolean {
+        return false
+    }
+
+    override fun getMessageBodySize(request: SendRequest<Data, Meta>?): Long? {
+        return null
+    }
+
+    override fun getMessageEnvelopeSize(request: SendRequest<Data, Meta>?): Long? {
+        return null
+    }
+
+    override fun getClientId(request: SendRequest<Data, Meta>?): String? {
+        return null
+    }
+
+    override fun getBatchMessageCount(request: SendRequest<Data, Meta>, response: SendResult<Data, Meta>?): Long? {
+        return null
     }
 }
 
@@ -64,16 +80,8 @@ internal class ConsumerResponseAttributesGetter<Data, Meta : Any>(private val qu
         return null
     }
 
-    override fun getMessagePayloadSize(request: List<Message<Data, Meta>>?): Long? {
-        return null
-    }
-
-    override fun getMessagePayloadCompressedSize(request: List<Message<Data, Meta>>?): Long? {
-        return null
-    }
-
-    override fun getMessageId(request: List<Message<Data, Meta>>?, response: Unit?): String? {
-        return if (request != null && request.size == 1) {
+    override fun getMessageId(request: List<Message<Data, Meta>>, response: Unit?): String? {
+        return if (request.size == 1) {
             request[0].id.toString()
         } else {
             null
@@ -87,6 +95,30 @@ internal class ConsumerResponseAttributesGetter<Data, Meta : Any>(private val qu
 
         return request.mapNotNull { it.openTelemetryData?.get(name) }
     }
+
+    override fun getDestinationTemplate(request: List<Message<Data, Meta>>?): String? {
+        return null
+    }
+
+    override fun isAnonymousDestination(request: List<Message<Data, Meta>>?): Boolean {
+        return false
+    }
+
+    override fun getMessageBodySize(request: List<Message<Data, Meta>>?): Long? {
+        return null
+    }
+
+    override fun getMessageEnvelopeSize(request: List<Message<Data, Meta>>?): Long? {
+        return null
+    }
+
+    override fun getClientId(request: List<Message<Data, Meta>>?): String? {
+        return null
+    }
+
+    override fun getBatchMessageCount(request: List<Message<Data, Meta>>, response: Unit?): Long? {
+        return null
+    }
 }
 
 
@@ -94,7 +126,7 @@ internal class SendRequestAttributesExtractor<Data, Meta : Any> : AttributesExtr
 
     override fun onStart(attributes: AttributesBuilder, parentContext: Context, request: SendRequest<Data, Meta>) {
         if (request.data.size > 1) {
-            attributes.put(SemanticAttributes.MESSAGING_BATCH_MESSAGE_COUNT, request.data.size.toLong())
+            attributes.put(MessagingIncubatingAttributes.MESSAGING_BATCH_MESSAGE_COUNT, request.data.size.toLong())
         }
     }
 
@@ -113,7 +145,7 @@ internal class ConsumerResponseAttributesExtractor<Data, Meta: Any>: AttributesE
 
     override fun onStart(attributes: AttributesBuilder, parentContext: Context, request: List<Message<Data, Meta>>) {
         if (request.size > 1) {
-            attributes.put(SemanticAttributes.MESSAGING_BATCH_MESSAGE_COUNT, request.size.toLong())
+            attributes.put(MessagingIncubatingAttributes.MESSAGING_BATCH_MESSAGE_COUNT, request.size.toLong())
         }
     }
 
