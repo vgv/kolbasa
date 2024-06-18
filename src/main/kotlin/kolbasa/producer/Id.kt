848d51bcf096af17c3f4b908da71d37b33b97ff7
Vasily Vasilkov <chand0s@yandex.ru>
2024-06-18 21:15:32 +0400

    Add commments

--- src/main/kotlin/kolbasa/producer/Id.kt
+++ src/main/kotlin/kolbasa/producer/Id.kt
@@ -2,6 +2,12 @@ package kolbasa.producer
 
 import kolbasa.schema.Const
 
+/**
+ * Full record id
+ *
+ * This class is a container for all the parts that make up an ID. Only all components, only full ID uniquely
+ * identifies the record. Any part doesn't make sense without the others.
+ */
 data class Id(
     val localId: Long,
     val serverId: String?
@@ -16,9 +22,7 @@ data class Id(
     }
 
     companion object {
-
-        val DEFAULT_DUPLICATE_ID = Id(Const.RESERVED_DUPLICATE_ID, null)
-
+        internal val DEFAULT_DUPLICATE_ID = Id(Const.RESERVED_DUPLICATE_ID, null)
     }
 
 }
