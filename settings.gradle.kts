0b8887d992b03eeb1bbc56a8a50a561a983bb48b
Aleksandr Turchenko <turchenko.alexander@gmail.com>
2024-06-23 20:41:22 +0400

    Move plugins from settings gradle to build gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,6 @@
 rootProject.name = "kolbasa"
 
 pluginManagement {
-    val kotlinVersion: String by settings
-    val nexusPublishVersion: String by settings
-    val nebulaReleaseVersion: String by settings
-
-    plugins {
-        id("org.jetbrains.kotlin.jvm") version kotlinVersion
-        id("io.github.gradle-nexus.publish-plugin") version nexusPublishVersion
-        id("com.netflix.nebula.release") version nebulaReleaseVersion
-    }
-
     repositories {
         gradlePluginPortal()
         mavenCentral()
