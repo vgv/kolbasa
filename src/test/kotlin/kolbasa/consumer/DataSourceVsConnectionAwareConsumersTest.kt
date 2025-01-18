48ea5a14b836abe64d8c4fb8be8aab0c8de4121e
Vasily Vasilkov <chand0s@yandex.ru>
2025-01-18 11:52:24 +0400

    Revert @Ignore

--- src/test/kotlin/kolbasa/consumer/DataSourceVsConnectionAwareConsumersTest.kt
+++ src/test/kotlin/kolbasa/consumer/DataSourceVsConnectionAwareConsumersTest.kt
@@ -3,14 +3,11 @@ package kolbasa.consumer
 import kolbasa.consumer.connection.ConnectionAwareConsumer
 import kolbasa.consumer.datasource.Consumer
 import kotlin.reflect.full.declaredMemberFunctions
-import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class DataSourceVsConnectionAwareConsumersTest {
 
-    // TODO: enable it again after full migration completed
-    @Ignore
     @Test
     fun testSameMethods() {
         // Test we have the same methods in these two interfaces
