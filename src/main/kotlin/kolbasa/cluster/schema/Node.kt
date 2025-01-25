4a704216c2d5e46251528522f081f372ad1d9dc7
Vasily Vasilkov <chand0s@yandex.ru>
2025-01-25 10:35:41 +0400

    Make node comparable

--- src/main/kotlin/kolbasa/cluster/schema/Node.kt
+++ src/main/kotlin/kolbasa/cluster/schema/Node.kt
@@ -3,4 +3,9 @@ package kolbasa.cluster.schema
 data class Node(
     val serverId: String,
     val identifierBucket: Int?
-)
+) : Comparable<Node> {
+
+    override fun compareTo(other: Node): Int {
+        return this.serverId.compareTo(other.serverId)
+    }
+}
