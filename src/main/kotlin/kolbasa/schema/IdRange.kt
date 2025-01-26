58ab3dcb95f0606d090f4d5ae5072931317d8883
Vasily Vasilkov <chand0s@yandex.ru>
2025-01-26 14:48:47 +0400

    Add identifiers ranges

--- src/main/kotlin/kolbasa/schema/IdRange.kt
+++ src/main/kotlin/kolbasa/schema/IdRange.kt
@@ -2,16 +2,16 @@ package kolbasa.schema
 
 import kolbasa.cluster.schema.Node
 
-internal data class IdentifierRange(
+internal data class IdRange(
     val start: Long,
     val end: Long
 ) {
 
     companion object {
-        fun generateRange(bucket: Int): IdentifierRange {
+        fun generateRange(bucket: Int): IdRange {
             val start = bucket.toLong() shl Node.BITS_TO_HOLD_ID_VALUE
             val end = start or Node.VALUE_MASK
-            return IdentifierRange(start, end)
+            return IdRange(start, end)
         }
     }
 
