c29fe289222ceec76847ee9d0a6b1eece78e54bd
Vasily Vasilkov <chand0s@yandex.ru>
2025-11-22 14:50:30 +0400

    Add test

---
+++ src/test/kotlin/kolbasa/utils/DaemonThreadFactoryTest.kt
@@ -0,0 +1,24 @@
+package kolbasa.utils
+
+import io.mockk.mockk
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class DaemonThreadFactoryTest {
+
+    @Test
+    fun testNewThread() {
+        // Two different factories create threads independently
+        DaemonThreadFactory("first_factory").newThread(mockk()).also { thread ->
+            assertTrue(thread.isDaemon)
+            assertEquals("first_factory-1", thread.name)
+        }
+
+        DaemonThreadFactory("second_factory").newThread(mockk()).also { thread ->
+            assertTrue(thread.isDaemon)
+            assertEquals("second_factory-2", thread.name)
+        }
+    }
+
+}
