f2009ca23235f2623113b7d9a1d5c467b780b060
Vasily Vasilkov <chand0s@yandex.ru>
2025-11-29 21:14:55 +0400

    Improve tests

--- src/test/kotlin/kolbasa/producer/datasource/DatabaseProducerDeduplicationTest.kt
+++ src/test/kotlin/kolbasa/producer/datasource/DatabaseProducerDeduplicationTest.kt
@@ -46,11 +46,12 @@ class DatabaseProducerDeduplicationTest : AbstractPostgresqlTest() {
         producer.send(queue, messageToSend)
 
         // Second send with the same meta field value should fail
-        val (failedMessages, result) = producer.send(queue, messageToSend)
-        assertEquals(1, failedMessages)
-        assertEquals(0, result.onlySuccessful().size) // zero really inserted
-        assertEquals(0, result.onlyDuplicated().size) // zero duplicates
-        assertEquals(1, result.onlyFailed().size)  // one error
+        producer.send(queue, messageToSend).let { (failedMessages, result) ->
+            assertEquals(1, failedMessages)
+            assertEquals(0, result.onlySuccessful().size) // zero really inserted
+            assertEquals(0, result.onlyDuplicated().size) // zero duplicates
+            assertEquals(1, result.onlyFailed().size)  // one error
+        }
 
         // raw database check
         assertEquals(1, dataSource.readInt("select count(*) from ${queue.dbTableName}"))
@@ -72,11 +73,12 @@ class DatabaseProducerDeduplicationTest : AbstractPostgresqlTest() {
             SendMessage("bugaga_$it", MetaValues.of(FIELD.value(it)))
         }
         // this call should not insert anything, because there is the poison message with TestMeta.field == 1
-        val sendResult = producer.send(queue, messagesWithPoisonMessage)
-        assertEquals(0, sendResult.onlySuccessful().size) // zero really inserted
-        assertEquals(0, sendResult.onlyDuplicated().size) // zero duplicates
-        assertEquals(1, sendResult.onlyFailed().size) // One error containing all 100 messages, because nothing was inserted
-        assertEquals(100, sendResult.failedMessages) // 100 messages failed, because the whole list failed
+        producer.send(queue, messagesWithPoisonMessage).let { sendResult ->
+            assertEquals(0, sendResult.onlySuccessful().size) // zero really inserted
+            assertEquals(0, sendResult.onlyDuplicated().size) // zero duplicates
+            assertEquals(1, sendResult.onlyFailed().size) // One error containing all 100 messages, because nothing was inserted
+            assertEquals(100, sendResult.failedMessages) // 100 messages failed, because the whole list failed
+        }
 
         // raw database check
         assertEquals(1, dataSource.readInt("select count(*) from ${queue.dbTableName}"))
@@ -94,11 +96,12 @@ class DatabaseProducerDeduplicationTest : AbstractPostgresqlTest() {
         producer.send(queue, messageToSend)
 
         // Second send with the same meta field value should return Const.RESERVED_DUPLICATE_ID and not insert anything
-        val (failedMessages, result) = producer.send(queue, messageToSend)
-        assertEquals(0, failedMessages)
-        assertEquals(0, result.onlySuccessful().size)
-        assertEquals(1, result.onlyDuplicated().size)
-        assertEquals(0, result.onlyFailed().size)
+        producer.send(queue, messageToSend).let { (failedMessages, result) ->
+            assertEquals(0, failedMessages)
+            assertEquals(0, result.onlySuccessful().size)
+            assertEquals(1, result.onlyDuplicated().size)
+            assertEquals(0, result.onlyFailed().size)
+        }
 
         // raw database check
         assertEquals(1, dataSource.readInt("select count(*) from ${queue.dbTableName}"))
@@ -119,11 +122,11 @@ class DatabaseProducerDeduplicationTest : AbstractPostgresqlTest() {
         val messagesWithPoisonMessage = (100 downTo 1).map {
             SendMessage("bugaga_$it", MetaValues.of(FIELD.value(it)))
         }
-        val sendResult = producer.send(queue, messagesWithPoisonMessage)
-
-        assertEquals(99, sendResult.onlySuccessful().size) // 99 really inserted
-        assertEquals(1, sendResult.onlyDuplicated().size) // 1 duplicate
-        assertEquals(0, sendResult.onlyFailed().size) // zero errors
+        producer.send(queue, messagesWithPoisonMessage).let { sendResult ->
+            assertEquals(99, sendResult.onlySuccessful().size) // 99 really inserted
+            assertEquals(1, sendResult.onlyDuplicated().size) // 1 duplicate
+            assertEquals(0, sendResult.onlyFailed().size) // zero errors
+        }
 
         // raw database check
         assertEquals(100, dataSource.readInt("select count(*) from ${queue.dbTableName}"))
