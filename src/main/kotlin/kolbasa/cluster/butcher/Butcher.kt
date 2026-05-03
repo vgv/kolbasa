50b613dd86e7c3221542feab91bd006ab971e2bd
Vasily Vasilkov <chand0s@yandex.ru>
2026-05-03 21:36:41 +0400

    Add colors

--- src/main/kotlin/kolbasa/cluster/butcher/Butcher.kt
+++ src/main/kotlin/kolbasa/cluster/butcher/Butcher.kt
@@ -9,7 +9,7 @@ fun main(args: Array<String>) {
         Command.parseCommand(args)
     } catch (e: ButcherException.InvalidConfigurationException) {
         println("=================================================")
-        println("Invalid configuration.")
+        println(red("Invalid configuration."))
         println(e.messageToShow)
         println("=================================================")
         exitProcess(1)
@@ -24,14 +24,26 @@ fun main(args: Array<String>) {
         }
     } catch (e: ButcherException.ExecutionException) {
         println("=================================================")
-        println("Execution error.")
+        println(red("Execution error."))
         println(e.messageToShow)
         println("=================================================")
         exitProcess(1)
     }
 
     println("=================================================")
-    println("Completed successfully.")
+    println(green("Completed successfully."))
     println(result)
     println("=================================================")
 }
+
+private fun red(msg: String): String {
+    return "$ANSI_RED$msg$ANSI_RESET"
+}
+
+private fun green(msg: String): String {
+    return "$ANSI_GREEN$msg$ANSI_RESET"
+}
+
+private const val ANSI_RESET = "\u001B[0m"
+private const val ANSI_RED = "\u001B[31m"
+private const val ANSI_GREEN = "\u001B[32m"
